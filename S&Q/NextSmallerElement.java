1c85ee73bae1e296fd91843fe8d9e26ace4ef7f7
Sonam Agrawal <64350434+sonam-ag@users.noreply.github.com>
2022-04-06 14:56:00 +0530

    Create NextSmallerElement.java

---
+++ S&Q/NextSmallerElement.java
@@ -0,0 +1,29 @@
+package stack;
+import java.util.*;
+public class NextSmallerElement {
+
+	public static int[] nexSmallerElement(int[] nums1, int[] nums2) {
+		Stack<Integer> stack = new Stack<Integer>();
+		HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+		int[] res = new int[nums1.length];
+		for(int i=nums2.length-1;i>=0 ; i--) {
+			while(!stack.isEmpty() && stack.peek() >= nums2[i]) {
+				stack.pop();
+			}
+			map.put(nums2[i], stack.isEmpty()?-1:stack.peek());
+			stack.push(nums2[i]);
+			
+		}
+		for(int i =0 ; i< nums1.length ; i ++) {
+			res[i] = map.get(nums1[i]);
+		}	
+		return res;      
+	}
+	public static void main(String[] args) {
+		int[] ans = NextSmallerElement.nexSmallerElement(new int[] {2,4}, new int[] {1,2,1,4,3});
+		for(Integer i : ans) {
+			System.out.println(i);
+		}
+	}
+
+}
